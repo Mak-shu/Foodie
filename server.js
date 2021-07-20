91aa08bab49284bcccf9285cbbca652d0377521d
Mak-shu <agarwalshubhi275@gmail.com>
2021-07-21 00:26:32 +0530

    Added cart and Registration

--- server.js
+++ server.js
@@ -14,18 +14,29 @@ const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
 
 //Assets
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-    res.render('home')              //resources/views/home
-})
-
+app.use(express.static('public'))   
 
 //set Template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
+//routes - should be below the template engine code
+app.get('/', (req, res) => {
+    res.render('home')                          //resources/views/home
+})
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')                //resources/views/customers/cart
+})
+
+app.get('/login', (req, res) => {
+    res.render('auth/login')                    //resources/views/auth/login
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register')                 //resources/views/auth/register
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port :  ${PORT}`)
